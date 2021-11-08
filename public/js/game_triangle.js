a66b7f708e4014ac35cee90374140e203c73931f
asurdina <a.surdina@warwick.ac.uk>
2021-11-08 17:25:50 +0100

    Adding game rooms that group players into pairs.

--- public/js/game_triangle.js
+++ public/js/game_triangle.js
@@ -1,3 +1,364 @@
+const gameRooms = {
+    // [roomKey]: {
+    //     users: [],
+    //     randomTasks: [],
+    //     scores: [],
+    //     gameScore: 0,
+    //     players: {},
+    //     numPlayers: 0
+    // }
+}
+var gameStarted = 0;
+
+
+class Game extends Phaser.Game {
+    constructor() {
+        super(config);
+
+        // add scenes here
+        this.scene.add("MainScene", MainScene);
+        this.scene.add("WelcomeScene", WelcomeScene);
+        this.scene.add("WaitingScene", WaitingScene);
+        this.scene.add("GameScene", GameScene);
+
+
+        // start first scene
+        this.scene.start("MainScene");
+    }
+
+    
+}
+
+
+
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super({key: "MainScene"});
+        this.state = {};
+    }
+
+    preload() {
+        this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+        this.load.image('background', 'graphics/background2.png');
+        this.load.image('ground', 'graphics/ground.png');
+        this.load.image('ship', 'graphics/triangle_grey.png');
+        this.load.image('vwall', 'graphics/vwall.png');
+        this.load.image('hwall', 'graphics/hwall.png');
+        this.load.image('orange_dot', 'graphics/orange_dot.png');
+        this.load.image('star', 'graphics/star.png');
+        this.load.image('purple_block', 'graphics/purple_block.png');
+        this.load.image('exit', 'graphics/exit2.png');
+        this.load.image('trap', 'graphics/trap_large.png');
+        this.load.image('trapButton', 'graphics/trap_switch.png');
+    }
+
+    create() {
+        this.socket = io();
+        const scene = this;
+        // console.log("this socket: " + this.socket.id);
+        this.socket.on("connect", function() {
+            console.log('io socket id assigned: ' + scene.socket.id);
+            scene.scene.launch("WelcomeScene", { socket: scene.socket });
+        });
+
+        
+
+
+        // this.socket.on("setState", function(roomInfo) {
+        //     console.log("setState: " + roomInfo);
+        //     const { roomKey, players, numPlayers } = roomInfo;
+        //     scene.physics.resume();
+
+
+        //     // TODO update to actual data sent in state info
+        //     // state
+        //     scene.state.roomKey = roomKey;
+        //     scene.state.players = players;
+
+        //     console.log(roomInfo);
+        //     if (roomInfo.numPlayers < 2) {
+        //         scene.scene.start("WaitingScene", { socket: scene.socket });
+        //     } else {
+        //         scene.scene.start("GameScene", { socket: scene.socket });
+        //     }
+        // });
+
+        // TODO: add other listeners
+
+    }
+}
+
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super({key: "GameScene"});
+    }
+
+    init(data) {
+        this.socket = data.socket;
+        this.roomInfo = data.roomInfo;
+        console.log("Room info: " + this.roomInfo);
+        this.players = data.roomInfo.players;
+    }
+
+    preload() {
+    }
+
+    create() {
+       const self = this;
+       console.log("game scene starting");
+       console.log("socket id when gameStarted: " + this.socket.id);
+  
+    
+        // extract info about other player from starting state
+        
+        this.otherPlayers = this.physics.add.group();
+        //var walls;
+        this.walls = this.physics.add.group();
+
+        Object.keys(self.players).forEach(function (id) {
+            if (self.players[id].playerId === self.socket.id) {
+                addPlayer(self, self.players[id]);
+            } else {
+                addOtherPlayers(self, self.players[id]);
+        }});
+      
+        this.socket.on("sendRoomKey", function(availableRoomKey) {
+            console.log("Joining room with roomKey: " + availableRoomKey);
+            self.socket.emit("joinRoom", availableRoomKey);
+        });
+    
+    
+        this.socket.on('currentPlayers', function (players) {
+            console.log(players);
+            Object.keys(players).forEach(function (id) {
+                if (players[id].playerId === self.socket.id) {
+                    addPlayer(self, players[id]);
+                } else {
+                    addOtherPlayers(self, players[id]);
+                }
+            });
+        });
+        // this.socket.on('newPlayer', function (playerInfo) {
+        //     addOtherPlayers(self, playerInfo);
+    
+        // });
+    
+        this.socket.on('disconnect', function (playerId) {
+            self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+                if (playerId === otherPlayer.playerId) {
+                    otherPlayer.destroy();
+                }
+            });
+        });
+    
+        this.socket.on('playerMoved', function (playerInfo) {
+            self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+                if (playerInfo.playerId === otherPlayer.playerId) {
+                    otherPlayer.setRotation(playerInfo.rotation);
+                    otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+                }
+            });
+        });
+    
+        this.cursors = this.input.keyboard.createCursorKeys();
+    
+        this.greenScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#1fc888' });
+        this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
+    
+        this.socket.on('scoreUpdate', function (scores) {
+            self.greenScoreText.setText('You: ' + scores.green);
+            self.redScoreText.setText('Other: ' + scores.red);
+        });
+    
+        this.socket.on('starLocation', function (starLocation) {
+            if (self.star) self.star.destroy();
+            self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
+            self.physics.add.overlap(self.ship, self.star, function () {
+                this.socket.emit('starCollected');
+            }, null, self);
+        });
+    
+        this.socket.on('trapLocation', function (trapLocation) {
+            if (self.trap) self.trap.destroy();
+            if (self.trapButton) self.trapButton.destroy();
+    
+            self.ship.trapped = false;
+    
+    
+            console.log('creating traps ...')
+            self.trap = new Trap(self, trapLocation.x, trapLocation.y);   
+    
+            self.physics.add.overlap(self.ship, self.trap, function () {
+                // do the following only if player was not trapped before
+                if (this.ship.trapped == false) {
+                    this.socket.emit('playerEntrapment');
+                    console.log('player trapped');
+                    activateTrap(self);
+                }
+            }, null, self);
+        });
+        
+        this.socket.on('trapButtonLocation', function (trapButtonLocation) {
+            if (self.trapButton) self.trapButton.destroy();
+            self.trapButton = new TrapButton(self, trapButtonLocation.x, trapButtonLocation.y);
+            self.physics.add.overlap(self.ship, self.trapButton, function() {
+              self.trapButton.destroy();
+              this.socket.emit('trapReleased');
+            }, null, self);
+        });
+    
+     
+    
+        this.socket.on('playerFreed', function () {
+            console.log('all players are freeeeeeeeeeeeeee');
+            if (this.trap) this.trap.destroy();
+        });
+    
+        // TODO: show waiting screen
+        // TODO: emit when player chooses to join game
+    
+    }
+
+    update() {
+
+        if (this.ship && this.ship.trapped == false) {
+    
+            if (this.cursors.left.isDown) {
+                this.ship.setAngularVelocity(-150);
+            } else if (this.cursors.right.isDown) {
+                this.ship.setAngularVelocity(150);
+            } else {
+                this.ship.setAngularVelocity(0);
+            }
+    
+            if (this.cursors.up.isDown) {
+                this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+            } else {
+                this.ship.setAcceleration(0);
+            }
+    
+            // no walls
+            //this.physics.world.wrap(this.ship, 5);
+    
+            // emit player movement
+            var x = this.ship.x;
+            var y = this.ship.y;
+            var r = this.ship.rotation;
+            if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+                this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+            }
+            // save old position data
+            this.ship.oldPosition = {
+                x: this.ship.x,
+                y: this.ship.y,
+                rotation: this.ship.rotation
+            };
+    
+        } else if (this.ship && this.ship.trapped == true) {
+            // make trap absorb player and only allow rotation
+            this.physics.moveToObject(this.ship, this.trap, 60, 300);
+    
+            if (this.cursors.left.isDown) {
+                this.ship.setAngularVelocity(-150);
+            } else if (this.cursors.right.isDown) {
+                this.ship.setAngularVelocity(150);
+            } else {
+                this.ship.setAngularVelocity(0);
+            }
+    
+            // emit player movement
+            x = this.ship.x;
+            y = this.ship.y;
+            r = this.ship.rotation;
+            if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+                this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+            }
+            // save old position data
+            this.ship.oldPosition = {
+                x: this.ship.x,
+                y: this.ship.y,
+                rotation: this.ship.rotation
+            };
+        }
+    }
+
+
+}
+
+
+class WaitingScene extends Phaser.Scene {
+    constructor() {
+        super({key: "WaitingScene"});
+    }
+
+    init(data) {
+        this.socket = data.socket;
+        // console.log("socket id in WaitingScene: ", this.socket.id);
+    }
+
+    create() {
+        const scene = this;
+        this.add.text(20, 20, 'Waiting for second player to join')
+
+        this.socket.on("startGame", function(availableRoomKey){
+            console.log("starting signal received! RoomKey: " + availableRoomKey);
+        });
+        this.socket.on("setStartingState", function(roomInfo) {
+            console.log("setStartingState: " + roomInfo);
+            // const { roomKey, players, numPlayers } = roomInfo;
+
+            
+            // scene.physics.resume();
+
+              // TODO update to actual data sent in state info
+            // state
+            // this.state.roomKey = roomKey;
+            // this.state.players = players;
+
+            scene.scene.start("GameScene", { socket: scene.socket,
+            roomInfo: roomInfo });
+
+            console.log(roomInfo);
+
+        });
+    }
+
+    update() {
+        
+        
+    }
+}
+class WelcomeScene extends Phaser.Scene {
+
+    constructor() {
+        super({key: "WelcomeScene"});
+
+    }
+
+    init(data) {
+        this.socket = data.socket;
+        // console.log("socket id in WelcomeScene: ", this.socket.id);
+    }
+
+    create() {
+        this.add.text(20, 20, 'Press any key to start game...')
+        this.input.keyboard.on('keydown', function() {
+            console.log("requesting to start gamescene");
+            gameStarted = 1;
+        });
+    }
+
+    update() {
+        if (gameStarted == 1) {
+            this.scene.start("WaitingScene", { socket: this.socket });
+            this.socket.emit("getRoomKey");
+
+        }
+
+
+    }
+}
+
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
@@ -16,215 +377,16 @@ var config = {
             }
         }
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: []
 };
 
-// new Phaser.Game(config);
-
-
-class Game extends Phaser.Game {
-    constructor() {
-        super(config);
-    }
-}
 
 // New game instance
 window.onload = function() {
     window.game = new Game();
 };
 
-function preload() {
-    this.load.image('otherPlayer', 'assets/enemyBlack5.png');
-    this.load.image('background', 'graphics/background2.png');
-    this.load.image('ground', 'graphics/ground.png');
-    this.load.image('ship', 'graphics/triangle_grey.png');
-    this.load.image('vwall', 'graphics/vwall.png');
-    this.load.image('hwall', 'graphics/hwall.png');
-    this.load.image('orange_dot', 'graphics/orange_dot.png');
-    this.load.image('star', 'graphics/star.png');
-    this.load.image('purple_block', 'graphics/purple_block.png');
-    this.load.image('exit', 'graphics/exit2.png');
-    this.load.image('trap', 'graphics/trap_large.png');
-    this.load.image('trapButton', 'graphics/trap_switch.png');
-}
 
-function create() {
-    var self = this;
-    this.socket = io();
-    
-    this.otherPlayers = this.physics.add.group();
-    //var walls;
-    this.walls = this.physics.add.group();
-    // this.traps = this.physics.add.group({
-    //     defaultKey: 'trap',
-    //     classType: Traps,
-    //     createCallback: function (trap) {
-    //         trap.setName('trap' + this.getLength());
-    //         console.log('Created', trap.name);
-    //     },
-    //     removeCallback: function (trap) {
-    //         console.log('Removed', trap.name);
-    //     }
-    // });
-
-
-
-    this.socket.on('currentPlayers', function (players) {
-        Object.keys(players).forEach(function (id) {
-            if (players[id].playerId === self.socket.id) {
-                addPlayer(self, players[id]);
-            } else {
-                addOtherPlayers(self, players[id]);
-            }
-        });
-    });
-    this.socket.on('newPlayer', function (playerInfo) {
-        addOtherPlayers(self, playerInfo);
-
-    });
-
-    this.socket.on('disconnect', function (playerId) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerId === otherPlayer.playerId) {
-                otherPlayer.destroy();
-            }
-        });
-    });
-
-    this.socket.on('playerMoved', function (playerInfo) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerInfo.playerId === otherPlayer.playerId) {
-                otherPlayer.setRotation(playerInfo.rotation);
-                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-            }
-        });
-    });
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.greenScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#1fc888' });
-    this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
-
-    this.socket.on('scoreUpdate', function (scores) {
-        self.greenScoreText.setText('You: ' + scores.green);
-        self.redScoreText.setText('Other: ' + scores.red);
-    });
-
-    this.socket.on('starLocation', function (starLocation) {
-        if (self.star) self.star.destroy();
-        self.star = self.physics.add.image(starLocation.x, starLocation.y, 'star');
-        self.physics.add.overlap(self.ship, self.star, function () {
-            this.socket.emit('starCollected');
-        }, null, self);
-    });
-
-    this.socket.on('trapLocation', function (trapLocation) {
-        if (self.trap) self.trap.destroy();
-        if (self.trapButton) self.trapButton.destroy();
-
-        self.ship.trapped = false;
-
-
-        console.log('creating traps ...')
-        self.trap = new Trap(self, trapLocation.x, trapLocation.y);   
-
-        self.physics.add.overlap(self.ship, self.trap, function () {
-            // do the following only if player was not trapped before
-            if (this.ship.trapped == false) {
-                this.socket.emit('playerEntrapment');
-                console.log('player trapped');
-                activateTrap(self);
-            }
-        }, null, self);
-    });
-    
-    this.socket.on('trapButtonLocation', function (trapButtonLocation) {
-        if (self.trapButton) self.trapButton.destroy();
-        self.trapButton = new TrapButton(self, trapButtonLocation.x, trapButtonLocation.y);
-        self.physics.add.overlap(self.ship, self.trapButton, function() {
-          self.trapButton.destroy();
-          this.socket.emit('trapReleased');
-        }, null, self);
-    });
-
- 
-
-    this.socket.on('playerFreed', function () {
-        console.log('all players are freeeeeeeeeeeeeee');
-        if (this.trap) this.trap.destroy();
-    });
-
-    // TODO: show waiting screen
-    // TODO: emit when player chooses to join game
-
-}
-
-function update() {
-
-    if (this.ship && this.ship.trapped == false) {
-
-        if (this.cursors.left.isDown) {
-            this.ship.setAngularVelocity(-150);
-        } else if (this.cursors.right.isDown) {
-            this.ship.setAngularVelocity(150);
-        } else {
-            this.ship.setAngularVelocity(0);
-        }
-
-        if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-        } else {
-            this.ship.setAcceleration(0);
-        }
-
-        // no walls
-        //this.physics.world.wrap(this.ship, 5);
-
-        // emit player movement
-        var x = this.ship.x;
-        var y = this.ship.y;
-        var r = this.ship.rotation;
-        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
-        }
-        // save old position data
-        this.ship.oldPosition = {
-            x: this.ship.x,
-            y: this.ship.y,
-            rotation: this.ship.rotation
-        };
-
-    } else if (this.ship && this.ship.trapped == true) {
-        // make trap absorb player and only allow rotation
-        this.physics.moveToObject(this.ship, this.trap, 60, 300);
-
-        if (this.cursors.left.isDown) {
-            this.ship.setAngularVelocity(-150);
-        } else if (this.cursors.right.isDown) {
-            this.ship.setAngularVelocity(150);
-        } else {
-            this.ship.setAngularVelocity(0);
-        }
-
-        // emit player movement
-        x = this.ship.x;
-        y = this.ship.y;
-        r = this.ship.rotation;
-        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
-        }
-        // save old position data
-        this.ship.oldPosition = {
-            x: this.ship.x,
-            y: this.ship.y,
-            rotation: this.ship.rotation
-        };
-    }
-}
 
 class Trap extends Phaser.Physics.Arcade.Sprite {
 
--- server.js
+++ server.js
@@ -20,16 +20,20 @@ const io = require('socket.io')(server, {
 });
 
 let players = {};
-let pairing_queue = {};
 
 const gameRooms = {
     // [roomKey]: {
     //     users: [],
-    //     gameScore: [],
-    //     players: [],
-    //     roundsPlayed: 0
+    //     randomTasks: [],
+    //     scores: [],
+    //     gameScore: 0,
+    //     players: {},
+    //     numPlayers: 0,
+    //     roundsPlayed: 0,
     // }
 };
+var availableRoomKey = "";
+
 
 let star = {
     x: Math.floor(Math.random() * 700) + 50,
@@ -54,24 +58,98 @@ let trapActive = false;
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+
+    // TODO: when player joins a room:
     // create a new player and add to players object
-    players[socket.id] = {
-        rotation: 0,
-        x: Math.floor(Math.random() * 700) + 50,
-        y: Math.floor(Math.random() * 500) + 50,
-        playerId: socket.id,
-        team: 'green',
-        trapped: false
-    };
-    // send players object to new player
-    socket.emit('currentPlayers', players);
+
+    // socket.on("joinRoom", function(roomKey) {
+    //     socket.join(roomKey);
+    //     const roomInfo = gameRooms[roomKey];
+    //     console.log("roomInfo", roomInfo);
+    //     // update players such that room key is stored in players object
+    //     roomInfo.players[socket.id] = {
+    //         rotation: 0,
+    //         x: Math.floor(Math.random() * 700) + 50,
+    //         y: Math.floor(Math.random() * 500) + 50,
+    //         playerId: socket.id,
+    //         team: 'green',
+    //         trapped: false        
+    //     };
+    //     socket.to(roomKey).emit("setStartingState", roomInfo);
+    //     console.log("sending setStartingState to roomKey " + roomKey)
+    // });
+
+    // players[socket.id] = {
+    //     rotation: 0,
+    //     x: Math.floor(Math.random() * 700) + 50,
+    //     y: Math.floor(Math.random() * 500) + 50,
+    //     playerId: socket.id,
+    //     team: 'green',
+    //     trapped: false
+    // };
+
 
 
     // TODO: build waiting room
+    // add player to queue
+    // when two players are in queue, game ID is generated
+    // and game starts automatically
+
+
+
+    // (backup option: if no partner found within 60 seconds, use AI?)
+    // => use pilot data to record how long matching usually takes
+
+    // Menu:
+    // join next available game
+    // join specific game
+    // join game by code
 
     // TODO: start new game here. 
     // Each game should have a separate game_id.
     // Generate star and trap coordinates for each game separately. 
+    
+
+
+    socket.on("getRoomKey", function() {
+
+        // get room ID of room with <2 players, or new room
+        availableRoomKey = assignRoomKey();
+        socket.emit("sendRoomKey", availableRoomKey);
+        socket.join(availableRoomKey);
+
+
+        let roomInfo = gameRooms[availableRoomKey];
+        roomInfo.players[socket.id] = {
+            rotation: 0,
+            x: Math.floor(Math.random() * 700) + 50,
+            y: Math.floor(Math.random() * 500) + 50,
+            playerId: socket.id,
+            team: 'green',
+            trapped: false,
+            roomKey : availableRoomKey,
+        };
+
+        // also store info in global players object
+        players[socket.id] = roomInfo.players[socket.id];
+        gameRooms[availableRoomKey].numPlayers += 1;
+        gameRooms[availableRoomKey] = roomInfo;
+        console.log("roomInfo", roomInfo);
+
+        if(roomInfo.numPlayers == 2) {
+            io.sockets.in(roomInfo.roomKey).emit("startGame", availableRoomKey);
+            console.log("## Sending start signal! \n## Room with roomKey " + availableRoomKey + " is good to go.");
+            io.sockets.in(roomInfo.roomKey).emit("setStartingState", roomInfo);
+             // send players object to players in rooom
+            io.sockets.in(roomInfo.roomKey).emit("currentPlayers", players);
+        }
+
+
+    });
+
+
+    // Emit room info when player joins a game
+
 
 
     // send star object to new player
@@ -88,7 +166,8 @@ io.on('connection', function (socket) {
     // create walls     
     socket.emit('createWalls', walls);
     // update all other players of the new player
-    socket.broadcast.emit('newPlayer', players[socket.id]);
+    // socket.broadcast.emit('newPlayer', players[socket.id]);
+
     console.log("socket.id: " + socket.id);
 
     socket.on('disconnect', function() {
@@ -104,8 +183,9 @@ io.on('connection', function (socket) {
         players[socket.id].x = movementData.x;
         players[socket.id].y = movementData.y;
         players[socket.id].rotation = movementData.rotation;
+
         // emit a message to all players about the player that moved
-        socket.broadcast.emit('playerMoved', players[socket.id]);
+        socket.broadcast.to(players[socket.id].roomKey).emit('playerMoved', players[socket.id]);
     });
     
     socket.on('starCollected', function () {
@@ -123,7 +203,7 @@ io.on('connection', function (socket) {
         players[socket.id].trapped = true;
 
         // emit a message to all players about the player that got trapped
-        socket.broadcast.emit('playerTrapped', players[socket.id]);
+        socket.broadcast.to(players[socket.id].roomKey).emit('playerTrapped', players[socket.id]);
         // create new button location only if nobody was in the trap before
         if (trapActive == false) {
             trapButton = generateLocation();
@@ -145,10 +225,6 @@ io.on('connection', function (socket) {
         // io.emit('scoreUpdate', scores);
     });
 
-    socket.on('joinGame', function() {
-        players[socket.id].is_playing = true;
-        players[socket.id].game_id = 0;
-    });
 
 });
 
@@ -159,3 +235,43 @@ function generateLocation() {
     };
 }
 
+function generateString() {
+    let my_string = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (let i = 1; i < 6; i++) {
+        my_string += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return my_string;
+}
+
+function createGameRoom() {
+     // generate room with new id
+     let key = generateString();
+     Object.keys(gameRooms).includes(key) ? (key = generateString()) : key;
+     gameRooms[key] = {
+         roomKey: key,
+         randomTasks: [],
+         gameScore: 0,
+         scores: {},
+         players: {},
+         numPlayers: 0,
+         roundsPlayed: 0,
+         starLocation: {},
+         trapLocation: {},
+         roomType: "",
+     };
+     return key;
+}
+
+// find rooms where game has not started yet, and only one player
+
+function assignRoomKey() {
+    availableRoomKey = "";
+    if (Object.values(gameRooms).some(e => e.numPlayers <= 1 && e.roundsPlayed == 0)) {
+        availableRoomKey = Object.values(gameRooms).find(e => e.numPlayers <= 1 && e.roundsPlayed == 0).roomKey;
+    } else {
+      // generate new room, put player there
+        availableRoomKey =  createGameRoom();
+    }
+    return availableRoomKey;
+}
